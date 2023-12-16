bd3d9b889d7c066441b1e871bc9ee2bccc3e43fc
agustinmassa <prueba@gmail.com>
2023-12-16 09:42:46 -0300

    carga de archivos

---
+++ run.py
@@ -0,0 +1,13 @@
+#El script principal que pondrá en marcha el programa.
+import ui
+import sys
+import menu
+
+if __name__=='__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == "-t":
+        menu.iniciar()
+    else:
+        app=ui.MainWindow()
+        app.mainloop()
+    
+
